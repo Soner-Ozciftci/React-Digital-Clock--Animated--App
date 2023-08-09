c944c59a25e3baf0978ac20758c90d2677c870e1
Soner-Ozciftci <ozciftcisoner@gmail.com>
2023-08-09 15:35:10 +0200

    LocaleTimeString

--- src/App.js
+++ src/App.js
@@ -2,9 +2,14 @@ import React, { useState } from "react";
 import "./App.css";
 
 function App() {
-  let time = new Date().toLocaleDateString();
+  let time = new Date().toLocaleTimeString();
 
   const [currentTime, setCurrentTime] = useState(time);
+
+  function Time() {
+    let time = new Date().toLocaleTimeString();
+    setCurrentTime(time);
+  }
   return (
     <div className="App">
       <h1>{currentTime}</h1>
